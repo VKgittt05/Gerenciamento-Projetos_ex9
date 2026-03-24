621de5a8420698dc2d01122aeb6af29bb0814dc1
Victor Kauã <victorelezeu@gmail.com>
2026-03-24 01:41:31 -0300

    Service

---
+++ Projetos/src/main/java/com/Gerenciamento_Projetos/Projetos/services/ProjetoService.java
@@ -0,0 +1,33 @@
+package com.Gerenciamento_Projetos.Projetos.services;
+
+import com.Gerenciamento_Projetos.Projetos.models.Projeto;
+import com.Gerenciamento_Projetos.Projetos.repositories.ProjetoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjetoService {
+
+    @Autowired
+    private ProjetoRepository repository;
+
+    public Projeto salvar(Projeto projeto) {
+        return repository.save(projeto);
+    }
+
+    public List<Projeto> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Projeto> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
+
